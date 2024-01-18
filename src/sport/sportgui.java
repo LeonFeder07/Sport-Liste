8de8ae8b57d0db957fb21ae13f505fca1d7f028b
leon.feder <leonfeder18@gmail.com>
2024-01-18 09:50:17 +0100

    we

--- src/sport/sportgui.java
+++ src/sport/sportgui.java
@@ -13,6 +13,10 @@ public class sportgui {
     private JButton hinzufügenButton;
     private JTextArea textlist;
     private JTextField textzeit;
+    private JButton löschenButton;
+    private JButton sucheschnellstenButton;
+    private JButton gibMirZeitButton;
+    private JTextField textField1;
 
 
     public sportgui() {
@@ -77,7 +81,58 @@ public class sportgui {
     );
 
 
-    }
+        sucheschnellstenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!teilnehmerliste.isEmpty()){
+                    teilnehmerliste.toFirst();
+                    double lokalezeit = teilnehmerliste.getContent().getZeit();
+                    String lokalername = teilnehmerliste.getContent().getVorname();
+                    while (teilnehmerliste.hasAccess()){
+                        teilnehmerliste.next();
+                        if (teilnehmerliste.getContent().getZeit()<lokalezeit && teilnehmerliste.hasAccess()){
+                            lokalezeit=teilnehmerliste.getContent().getZeit();
+                            lokalername=teilnehmerliste.getContent().getVorname();
+
+                        }
+
+                    }
+                    textField1.setText(lokalezeit+" "+ lokalername);
+                }
+            }
+        });
+        löschenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!teilnehmerliste.isEmpty()){
+                    teilnehmerliste.toFirst();
+                    while (teilnehmerliste.hasAccess()){
+                        while (teilnehmerliste.getContent().getVorname()!=textvorname.getText()){
+                            teilnehmerliste.next();
+                        }
+                        teilnehmerliste.remove();
+                    }
+                }
+            }
+        });
+        gibMirZeitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!teilnehmerliste.isEmpty()){
+                    teilnehmerliste.toFirst();
+                    while (teilnehmerliste.hasAccess()){
+                        while (teilnehmerliste.getContent().getVorname()!=textvorname.getText()){
+                            teilnehmerliste.next();
+                        }
+                       double lokalezeit1= teilnehmerliste.getContent().getZeit();
+                    }
+
+
+            }
+        });
+        }}
+
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("sportgui");
